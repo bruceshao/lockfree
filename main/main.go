ee6f4ce5550e48816bac859077b293f615be8ee5
shaozhuguang <leonzgshao@tencent.com>
2023-04-11 15:08:37 +0800

    add test for chan and lockfree in main.go

--- main/main.go
+++ main/main.go
@@ -19,25 +19,67 @@ import (
 var (
 	goSize    = 10000
 	sizePerGo = 10000
+	cap       = 1024 * 1024
 )
 
 func main() {
-	//runtime.GOMAXPROCS(10)
 	f, _ := os.OpenFile("cpu.pprof", os.O_CREATE|os.O_RDWR, 0644)
 	defer f.Close()
 	pprof.StartCPUProfile(f)
 	defer pprof.StopCPUProfile()
-	// lockfree计时
-	t := time.Now()
+	arg := ""
+	if len(os.Args) > 1 {
+		arg = os.Args[1]
+	}
+	if len(arg) == 0 {
+		fmt.Println("start lockfree and channel test")
+		// 表示全部启动
+		var wg sync.WaitGroup
+		wg.Add(2)
+		go func() {
+			defer wg.Done()
+			lockfreeMain()
+		}()
+		go func() {
+			defer wg.Done()
+			chanMain()
+		}()
+		wg.Wait()
+	} else if arg == "chan" {
+		fmt.Println("start channel test")
+		// 表示启动chan
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			chanMain()
+		}()
+		wg.Wait()
+	} else if arg == "lockfree" {
+		fmt.Println("start lockfree test")
+		// 表示启动lockfree
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			lockfreeMain()
+		}()
+		wg.Wait()
+	}
+	fmt.Println("all queue is over")
+}
+
+func lockfreeMain() {
 	// 创建事件处理器
 	eh := &longEventHandler[uint64]{}
 	// 创建消费端串行处理的Lockfree
-	lf := lockfree.NewLockfree[uint64](1024*1024, eh,
-		lockfree.NewSleepBlockStrategy(time.Microsecond))
+	lf := lockfree.NewLockfree[uint64](cap, eh, lockfree.NewSleepBlockStrategy(time.Millisecond))
 	// 启动Lockfree
 	if err := lf.Start(); err != nil {
 		panic(err)
 	}
+	// lockfree计时
+	ts := time.Now()
 	// 获取生产者对象
 	producer := lf.Producer()
 	var wg sync.WaitGroup
@@ -55,11 +97,47 @@ func main() {
 		}(i)
 	}
 	wg.Wait()
-	fmt.Println("=====lockfree[", time.Now().Sub(t), "]=====")
+	fmt.Println("=====lockfree[", time.Now().Sub(ts), "]=====")
 	fmt.Println("----- lockfree write complete -----")
-	time.Sleep(1 * time.Second)
+	time.Sleep(3 * time.Second)
 	// 关闭Lockfree
 	lf.Close()
+}
+
+func chanMain() {
+	c := make(chan uint64, cap)
+	// 启动监听协程
+	go func() {
+		for {
+			x, ok := <-c
+			if !ok {
+				return
+			}
+			if x%10000000 == 0 {
+				fmt.Println("chan [", x, "]")
+			}
+		}
+	}()
+	// lockfree计时
+	ts := time.Now()
+	// 开始写入
+	var wg sync.WaitGroup
+	wg.Add(goSize)
+	for i := 0; i < goSize; i++ {
+		go func(start int) {
+			for j := 0; j < sizePerGo; j++ {
+				//写入数据
+				c <- uint64(start*sizePerGo + j + 1)
+			}
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	fmt.Println("=====channel[", time.Now().Sub(ts), "]=====")
+	fmt.Println("----- channel write complete -----")
+	time.Sleep(3 * time.Second)
+	// 关闭Lockfree
+	close(c)
 }
 
 type longEventHandler[T uint64] struct {
