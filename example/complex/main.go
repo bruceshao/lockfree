e684adafbcd7e99db48b9bc6b58b2d066b1e8c36
shaozhuguang <leonzgshao@tencent.com>
2023-05-04 11:24:44 +0800

    feat: add write by cursor and disacrd

---
+++ example/complex/main.go
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package main
+
+import (
+	"fmt"
+	"github.com/bruceshao/lockfree"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+func main() {
+	fmt.Println("========== start write by discard ==========")
+	writeByDiscard()
+	fmt.Println("========== complete write by discard ==========")
+	fmt.Println("========== start write by cursor ==========")
+	writeByCursor()
+	fmt.Println("========== complete write by cursor ==========")
+}
+
+func writeByDiscard() {
+	var counter = uint64(0)
+	// 写入超时，如何使用
+	eh := &fixedSleepEventHandler[uint64]{
+		sm: time.Millisecond * 10,
+	}
+	disruptor := lockfree.NewLockfree[uint64](2, eh, lockfree.NewSleepBlockStrategy(time.Microsecond))
+	disruptor.Start()
+	producer := disruptor.Producer()
+	// 假设有100个写g
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			for j := 0; j < 10; j++ {
+				v := atomic.AddUint64(&counter, 1)
+				ww := producer.WriteWindow()
+				if ww <= 0 {
+					// 表示无法写入，丢弃
+					fmt.Println("discard ", v)
+					continue
+				}
+				// 表示可以写入，写入即可
+				err := producer.Write(v)
+				if err != nil {
+					panic(err)
+				}
+				fmt.Println("write ", v)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	time.Sleep(3 * time.Second)
+	disruptor.Close()
+}
+
+func writeByCursor() {
+	var counter = uint64(0)
+	// 写入超时，如何使用
+	eh := &randomSleepEventHandler[uint64]{}
+	disruptor := lockfree.NewLockfree[uint64](2, eh, lockfree.NewSleepBlockStrategy(time.Microsecond))
+	disruptor.Start()
+	producer := disruptor.Producer()
+	// 假设有100个写g
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			for j := 0; j < 10; j++ {
+				v := atomic.AddUint64(&counter, 1)
+				wc, exist, err := producer.WriteTimeout(v, time.Millisecond)
+				if err != nil {
+					return
+				}
+				if !exist {
+					// 重复写入1次，写入不成功则丢弃重新写其他的
+					if ok, _ := producer.WriteByCursor(v, wc); ok {
+						continue
+					}
+					fmt.Println("discard ", v)
+					// 重新生成值，一直等待写入
+					v = atomic.AddUint64(&counter, 1)
+					for {
+						if ok, _ := producer.WriteByCursor(v, wc); ok {
+							fmt.Println("write ", v, " with x times")
+							break
+						}
+						// 写入不成功则休眠，防止CPU暴增
+						time.Sleep(100 * time.Microsecond)
+					}
+				} else {
+					fmt.Println("write ", v, " with 1 time")
+				}
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	time.Sleep(3 * time.Second)
+	disruptor.Close()
+}
+
+
+type fixedSleepEventHandler[T uint64] struct {
+	sm time.Duration
+}
+
+func (h *fixedSleepEventHandler[T]) OnEvent(v uint64) {
+	time.Sleep(h.sm)
+	fmt.Println("consumer ", v)
+}
+
+type randomSleepEventHandler[T uint64] struct {
+	count int32
+}
+
+func (h *randomSleepEventHandler[T]) OnEvent(v uint64) {
+	// 每次处理都会进行随机休眠，可以导致消费端变慢
+	intn := rand.Intn(1000)
+	time.Sleep(time.Duration(intn * 1000))
+	fmt.Println("consumer count ", atomic.AddInt32(&h.count, 1))
+}
--- producer_test.go
+++ producer_test.go
@@ -9,6 +9,7 @@ package lockfree
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"reflect"
 	"sync"
@@ -123,4 +124,100 @@ func TestB(t *testing.T) {
 func TestC(t *testing.T) {
 	x := 128 - unsafe.Sizeof(uint64(0))%128
 	fmt.Println(x)
+}
+
+func TestProducer_WriteWindow(t *testing.T) {
+	eh := &sleepEventHandler[uint64]{
+		sm: time.Second,
+	}
+	disruptor := NewLockfree[uint64](1, eh,
+		NewSleepBlockStrategy(time.Microsecond))
+	disruptor.Start()
+	producer := disruptor.Producer()
+	// 写入10个数：0-9
+	// 预期结果，0可以写入，写入后在1ms内被取走，此时0会在等待1s后被打印，但由于ringbuffer有空位，所以1可以被写入
+	// 1写入后一直无法被取走，因为在等待1s内0的打印，后续其他值均无法被写入，因为1导致ringbuffer满了
+	for i := 0; i < 10; i++ {
+		ww := producer.WriteWindow()
+		if ww <= 0 {
+			// 表示不能写入，丢弃
+			fmt.Println("discard ", i , " window ", ww)
+		} else {
+			// 实际写入
+			producer.Write(uint64(i))
+		}
+		// 为了给予consumer时间取走
+		time.Sleep(time.Millisecond)
+	}
+	// 为了可以看到打印的结果
+	time.Sleep(3 * time.Second)
+	disruptor.Close()
+}
+
+func TestWriteTimeout(t *testing.T) {
+	var counter = uint64(0)
+	// 写入超时，如何使用
+	eh := &longSleepEventHandler[uint64]{}
+	disruptor := NewLockfree[uint64](2, eh, NewSleepBlockStrategy(time.Microsecond))
+	disruptor.Start()
+	producer := disruptor.Producer()
+	// 假设有10个写g
+	var wg sync.WaitGroup
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			for j := 0; j < 100; j++ {
+				v := atomic.AddUint64(&counter, 1)
+				wc, exist, err := producer.WriteTimeout(v, time.Millisecond)
+				if err != nil {
+					return
+				}
+				if !exist {
+					// 重复写入1次，写入不成功则丢弃重新写其他的
+					if ok, _ := producer.WriteByCursor(v, wc); ok {
+						continue
+					}
+					fmt.Println("discard ", v)
+					// 重新生成值，一直等待写入
+					v = atomic.AddUint64(&counter, 1)
+					for {
+						if ok, _ := producer.WriteByCursor(v, wc); ok {
+							fmt.Println("write ", v, " with x times")
+							break
+						}
+						// 写入不成功则休眠，防止CPU暴增
+						time.Sleep(100 * time.Microsecond)
+					}
+				} else {
+					fmt.Println("write ", v, " with 1 time")
+				}
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	time.Sleep(3 * time.Second)
+	disruptor.Close()
+}
+
+
+// sleepEventHandler 休眠性质的事件处理器
+type sleepEventHandler[T uint64] struct {
+	sm time.Duration
+}
+
+func (h *sleepEventHandler[T]) OnEvent(v uint64) {
+	time.Sleep(h.sm)
+	fmt.Println("consumer ", v)
+}
+
+type longSleepEventHandler[T uint64] struct {
+	count int32
+}
+
+func (h *longSleepEventHandler[T]) OnEvent(v uint64) {
+	// 每次处理都会进行随机休眠，可以导致消费端变慢
+	intn := rand.Intn(1000)
+	time.Sleep(time.Duration(intn * 1000))
+	fmt.Println("consumer count ", atomic.AddInt32(&h.count, 1))
 }
