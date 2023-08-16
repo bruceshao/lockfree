4ea209dee62533258e6e959bdad6e2182abca3da
shaozhuguang <leonzgshao@tencent.com>
2023-08-16 16:56:08 +0800

    fix 386 ringbuffer

--- buffer_386.go
+++ buffer_386.go
@@ -1,4 +1,4 @@
-//go:build !386
+//go:build 386
 
 /*
  * Copyright (C) THL A29 Limited, a Tencent company. All rights reserved.
@@ -9,7 +9,9 @@
 
 package lockfree
 
-import "sync/atomic"
+import (
+	"sync"
+)
 
 type e[T any] struct {
 	c   uint64
@@ -19,6 +21,7 @@ type e[T any] struct {
 // ringBuffer 具体对象的存放区域，通过数组（定长切片）实现环状数据结构
 // 其中e为具体对象，非指针，这样可以一次性进行内存申请
 type ringBuffer[T any] struct {
+	sync.RWMutex
 	// 增加默认的对象以便于return，处理data race问题
 	tDefault T
 	buf      []e[T]
@@ -35,8 +38,10 @@ func newRingBuffer[T any](cap int) *ringBuffer[T] {
 
 func (r *ringBuffer[T]) write(c uint64, v T) {
 	x := &r.buf[c&r.capMask]
+	r.Lock()
+	defer r.Unlock()
 	x.val = v
-	atomic.StoreUint64(&x.c, c+1)
+	x.c = c + 1
 }
 
 func (r *ringBuffer[T]) element(c uint64) e[T] {
@@ -45,7 +50,9 @@ func (r *ringBuffer[T]) element(c uint64) e[T] {
 
 func (r *ringBuffer[T]) contains(c uint64) (T, *uint64, bool) {
 	x := &r.buf[c&r.capMask]
-	if atomic.LoadUint64(&x.c) == c+1 {
+	r.RLock()
+	defer r.RUnlock()
+	if x.c == c+1 {
 		v := x.val
 		return v, &x.c, true
 	}
--- buffer_test.go
+++ buffer_test.go
@@ -62,3 +62,18 @@ func TestBuffer(t *testing.T) {
 	x = buf.element(1024)
 	fmt.Println(x)
 }
+
+func TestBufferAlign32(t *testing.T) {
+	buf := newRingBuffer[uint32](1024)
+	buf.write(0, 1)
+	x := buf.element(0)
+	fmt.Println(x)
+
+	buf.write(1023, 2)
+	x = buf.element(1023)
+	fmt.Println(x)
+
+	buf.write(1024, 3)
+	x = buf.element(1024)
+	fmt.Println(x)
+}
